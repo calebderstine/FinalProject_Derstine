f4eeb4bf2780760741e25d1131505652c37e96fd
calebderstine <calebderstine@gmail.com>
2025-04-14 10:58:40 -0400

    Development 2c

--- script.js
+++ script.js
@@ -3,16 +3,11 @@
 import Voice from "./Voice.js";
 
 //--------------------------Retrieve HTML Elements--------------------------
-let timeSig = document.getElementById("TimeSig");
 let tempoInput = document.getElementById("TempoInput");
 let transpositionInput = document.getElementById("TranspositionInput");
-let delayInput = document.getElementById("DelayInput");
-let feedback = document.getElementById("Feedback");
 let attackInput = document.getElementById("AttackInput");
 let decayInput = document.getElementById("DecayInput");
 let releaseInput = document.getElementById("ReleaseInput");
-let button = document.getElementById("StartStop");
-let masterGainSlider = document.getElementById("MasterGain");
 
 //------------------------Create Web Audio Objects------------------------
 /**
@@ -38,9 +33,9 @@ const audioCtx = new AudioContext();
  */
 const masterGain = audioCtx.createGain();
 masterGain.gain.value = 0.125; // Set master volume
-const updateMasterGain = function (event) {
+document.getElementById("MasterGain").addEventListener("input", (event)=>{
     masterGain.gain.linearRampToValueAtTime(event.target.value, audioCtx.currentTime + 0.4);
-};
+}); // Master Gain control
 
 /**
  * @constant {DelayNode} delayNode
@@ -52,17 +47,22 @@ const delayNode = audioCtx.createDelay();
 const feedbackNode = audioCtx.createGain();
 delayNode.delayTime.value = 0.003; // Set delay time
 feedbackNode.gain.value = 0; // Set feedback gain
-const updateDelayTime = function (event) {
+document.getElementById("DelayInput").addEventListener("input", (event)=>{
     delayNode.delayTime.linearRampToValueAtTime(event.target.value/1000, audioCtx.currentTime + 0.4);
-};
-const updateFeedback = function (event) {
+}); // Delay Time control
+document.getElementById("Feedback").addEventListener("input", (event)=>{
     feedbackNode.gain.linearRampToValueAtTime(event.target.value, audioCtx.currentTime + 0.4);
-};
+}); // Delay Feedback control
 // Maybe add a low pass filter to the delayed signal
 
+// Metronome
+const pulseGain = audioCtx.createGain();
+const subdivisionGain = audioCtx.createGain();
+pulseGain.gain.value = 1;
+subdivisionGain.gain.value = 0;
 // Define an asynchronous function to load and play the audio
 let source;
-const loadPlayMetronome = async ()=> {
+const loadPlayMetronome = async (out)=> {
     //fetch the audio file - returns a promise (response) so we await its completion
     const file = await fetch("metronome.mp3");
     //convert the fetched file into an arraybuffer (raw binary data)
@@ -75,20 +75,28 @@ const loadPlayMetronome = async ()=> {
     //attach the decoded audio data to the source node
     source.buffer = audioBuffer;
     //connect the audio source to the master gain (volume control)
-    source.connect(masterGain);
+    source.connect(out);
     //start playing the audio immediately
     source.start();
 };
+document.getElementById("PulseGain").addEventListener("input", (event)=>{
+    pulseGain.gain.linearRampToValueAtTime(event.target.value, audioCtx.currentTime + 0.4);
+}); // Pulse Gain control
+document.getElementById("SubdivisionGain").addEventListener("input", (event)=>{
+    subdivisionGain.gain.linearRampToValueAtTime(event.target.value, audioCtx.currentTime + 0.4);
+}); // Subdivision Gain control
 
 //------------------------------Connections-----------------------------------
 delayNode.connect(feedbackNode);
 feedbackNode.connect(delayNode);
 feedbackNode.connect(masterGain);
+pulseGain.connect(masterGain);
+subdivisionGain.connect(masterGain);
 masterGain.connect(audioCtx.destination);
 
 //----------------------------Define Variables----------------------------
 let numBeats = 4; // This will be determined by Time Signature.
-let meterType = "simple"; // This will be determined by Time Signature.
+let subdivision = 2; // This will be determined by Time Signature.
 let transposition; // This will be determined by Transposition selection.
 let duration; // This will be determined by Time Signature and Tempo selection.
 let attack; // This will be determined by Attack selection.
@@ -114,77 +122,74 @@ const selectedChords = [
  * @description Maps chord names to chord tones.
  */
 const chordMap = {
-    'i': [60, 63, 67],
-    'I': [60, 64, 67],
-    'bII': [61, 65, 68],
-    'ii*': [62, 65, 68],
-    'ii': [62, 65, 69],
-    'II': [62, 66, 69],
-    'III': [63, 67, 70],
-    'III+': [63, 67, 71],
-    'iii': [64, 67, 71],
-    'iv': [65, 68, 72],
-    'IV': [65, 69, 72],
-    'v': [67, 70, 74],
-    'V': [67, 71, 74],
-    'VI': [68, 72, 75],
-    'vi': [69, 72, 76],
-    'VII': [70, 74, 77],
-    'vii*': [71, 74, 77],
+    'i': [0, 3, 7],
+    'I': [0, 4, 7],
+    'bII': [1, 5, 8],
+    'ii*': [2, 5, 8],
+    'ii': [2, 5, 9],
+    'II': [2, 6, 9],
+    'III': [3, 7, 10],
+    'III+': [3, 7, 11],
+    'iii': [4, 7, 11],
+    'iv': [5, 8, 0],
+    'IV': [5, 9, 0],
+    'v': [7, 10, 2],
+    'V': [7, 11, 2],
+    'VI': [8, 0, 3],
+    'vi': [9, 0, 4],
+    'VII': [10, 2, 5],
+    'vii*': [11, 2, 5],
   };
 
 let currentChord;
 let randomIndex1;
 let randomIndex2;
 
-let voice1;
-let voice2;
-let voice3;
-let voice4;
+let currentVoicing = [];
 
 /**
  * @function updateTimeSig
  * @description Updates the time signature and number of beats per measure based on the selection from the HTML dropdown menu.
  */
-const updateTimeSig = function () {
-    switch (timeSig.value) {
+document.getElementById("TimeSig").addEventListener("input", ()=>{
+    switch (document.getElementById("TimeSig").value) {
         case "1":   // 4/4
             numBeats = 4;
-            meterType = "simple";
+            subdivision = 2;
             console.log(`${numBeats} beats`);
             break;
         case "2":   // 2/4
             numBeats = 2;
-            meterType = "simple";
+            subdivision = 2;
             console.log(`${numBeats} beats`);
             break;
         case "3":   // 3/4
             numBeats = 3;
-            meterType = "simple";
+            subdivision = 2;
             console.log(`${numBeats} beats`);
             break;
         case "4":   // 5/4
             numBeats = 5;
-            meterType = "simple";
+            subdivision = 2;
             console.log(`${numBeats} beats`);
             break;
         case "5":   // 6/8
             numBeats = 2;
-            meterType = "compound";
+            subdivision = 3;
             console.log(`${numBeats} beats`);
             break;
         case "6":   // 9/8
             numBeats = 3;
-            meterType = "compound";
+            subdivision = 3;
             console.log(`${numBeats} beats`);
             break;
         case "7":   // 12/8
             numBeats = 4;
-            meterType = "compound";
+            subdivision = 3;
             console.log(`${numBeats} beats`);
             break;
     }
-};
+});
 
 /**
  * @function mtof
@@ -214,13 +219,47 @@ const chooseChord = ()=> {
     document.getElementById("upcomingChord").innerText = currentChord;
 };
 
-const chooseVoicing = ()=> {
-    voice1 = chordMap[currentChord][0];
-    voice2 = chordMap[currentChord][1];
-    voice3 = chordMap[currentChord][2];
-    voice4 = chordMap[currentChord][0] + 12;
+const chooseVoicing2 = ()=> {
+    let currentVoicingClass = currentVoicing.map((num)=>{
+        return num % 12;
+    });
+    console.log(currentVoicingClass);
+    let chordVoiced;
+    while (!chordVoiced) {
+        chordVoiced = true;
+        chordMap[currentChord].forEach((value) => {
+            if (!currentVoicingClass.includes(value)) {
+                chordVoiced = false;
+            };
+        });
+        console.log(chordVoiced);
+    };
+
+    currentVoicing[0] = chordMap[currentChord][0] + 60;
+    currentVoicing[1] = chordMap[currentChord][1] + 60;
+    currentVoicing[2] = chordMap[currentChord][2] + 60;
+    currentVoicing[3] = chordMap[currentChord][0] + 72;
 };
 
+
+
+const chooseVoicing = ()=>{
+    currentVoicing[0] = chordMap[currentChord][0] + 60;
+    currentVoicing[1] = chordMap[currentChord][1] + 60;
+    currentVoicing[2] = chordMap[currentChord][2] + 60;
+    currentVoicing[3] = chordMap[currentChord][0] + 72;
+};
+
+//Async Function
+//Step 1    keep shared notes?
+//Step 2    enter a for loop with the condition that an array of the four voices !includes
+//          all elements of the currentChord array (will repeat until all chord tones are
+//          represented; will also be bypassed if the chord remains the same); for each new
+//          iteration, add 1 to a value that will represent the starting index of iteration
+//Step 3    using a for (let k = 0(or 1?); k < 12(or 6?); k++), where k represents a distance
+//          of semitones, iterate through each remaining voice and through the currentChord
+//          tones to 
+//(ignore parallel 5ths and 8ves and voice ranges for now)
 
 /**
  * @function playNote
@@ -237,7 +276,7 @@ const playNote = function (note) {
  * @description Will use a for loop to run through the process of choosing a chord, choosing the voicing and assigning notes to each voice, and starting the oscillators before repeating after the specified duration.
  * This function will call other functions for each specific task.
  */
-const startTrack = function () {
+document.getElementById("StartStop").addEventListener("click", ()=>{
     if (isPlaying) {
         clearTimeout(timeout); // Clears all timeouts because all timeouts share this same ID
         isPlaying = false;
@@ -250,7 +289,6 @@ const startTrack = function () {
     sustainTime = duration - attack - decay - release; // Any time remaining from the duration after the Attack, Decay, and Release is set to the time the note sustains
     transposition = parseInt(transpositionInput.value); // Transposition value is set to the value in the corresponding HTML input box // Might have to use parseInt()
     // By setting these values only when we press play, we ensure the values don't change mid-playback and break the playback loop
-    console.log(`${attack}, ${decay}, ${release}, ${sustainTime}, ${duration}`);
     if (sustainTime < 0) {
         console.log(
             "Decrease Attack, Decay, or Release time OR Decrease tempo" // MAKE IT OUTPUT A MESSAGE TO THE USER
@@ -258,53 +296,47 @@ const startTrack = function () {
         return;
     }; // This prevents the playback loop from starting if the sustain time would be less than 0, which would otherwise break the loop
     currentChord = selectedChords[0][0];
-    chooseVoicing();
+    currentVoicing[0] = chordMap[currentChord][0] + 60;
+    currentVoicing[1] = chordMap[currentChord][1] + 60;
+    currentVoicing[2] = chordMap[currentChord][2] + 60;
+    currentVoicing[3] = chordMap[currentChord][0] + 72;
     isPlaying = true;
-    const startTime = audioCtx.currentTime;
-        // iterate through numBeats
         for (let i = 0; i < numBeats; i++) {
             timeout = setTimeout(()=>{
-                loadPlayMetronome();
-                //console.log("click");
+                loadPlayMetronome(pulseGain);
               }, (duration/numBeats) * i * 1000)
         };
         console.log(currentChord);
         playback();
-};
+});
 
 const playback = function () {
     timeout = setTimeout(()=>{
         playback();
         for (let i = 0; i < numBeats; i++) {
             timeout = setTimeout(()=>{
-                loadPlayMetronome();
-                //console.log("click");
+                loadPlayMetronome(pulseGain);
               }, (duration/numBeats) * i * 1000)
         };
-        playNote(voice1);
-        playNote(voice2);
-        playNote(voice3);
-        playNote(voice4);
+        for (let i = 0; i < (numBeats*subdivision); i++) {
+            timeout = setTimeout(()=>{
+                loadPlayMetronome(subdivisionGain);
+              }, (duration/numBeats)/subdivision * i * 1000)
+        };
+        for (let i = 0; i < 4; i++) {
+            playNote(currentVoicing[i]);
+        };
         chooseChord();
         console.log(currentChord);
         chooseVoicing();
-        console.log(voice1, voice2, voice3, voice4);
+        console.log(currentVoicing[0], currentVoicing[1], currentVoicing[2], currentVoicing[3]);
     }, duration * 1000);
 };
-
-
-//Add Event Listeners
-timeSig.addEventListener("input", updateTimeSig);
-delayInput.addEventListener("input", updateDelayTime);
-feedback.addEventListener("input", updateFeedback);
-button.addEventListener("click", startTrack);
-masterGainSlider.addEventListener("input", updateMasterGain);
 
 //User Chord Selection Functionality
 document.getElementById("ChordSelection").addEventListener("click", (event)=>{
     let functIndex;
     if (allChords.includes(`${event.target.innerHTML}`)) {
-        console.log(event.target.innerHTML);
         switch (event.target.innerHTML) {
             case 'i': functIndex = 0; break;
             case 'I': functIndex = 0; break;
@@ -324,16 +356,12 @@ document.getElementById("ChordSelection").addEventListener("click", (event)=>{
             case 'VII': functIndex = 2; break;
             case 'vii*': functIndex = 2; break;
         }
-        console.log(functIndex);
         if (selectedChords[functIndex].includes(`${event.target.innerHTML}`)) {
-            console.log("includes");
             selectedChords[functIndex].splice(selectedChords[functIndex].indexOf(event.target.innerHTML), 1);
             event.target.style.backgroundColor = "#FFFAF0";
         } else {
-            console.log("doesn't include");
             selectedChords[functIndex].push(event.target.innerHTML);
             event.target.style.backgroundColor = "#696969";
         }
-        console.log(selectedChords);
     };
 });
